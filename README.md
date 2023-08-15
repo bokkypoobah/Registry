e5edd90c7ee62796fbf6a5f6b00098672d85ca6a
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-08-15 19:30:09 +1000

    Gas reporter

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ See https://bokkypoobah.github.io/Registry/
 https://hardhat.org/hardhat-runner/docs/guides/project-setup
 
 npm install --save-dev hardhat
+
+https://www.npmjs.com/package/hardhat-gas-reporter
+
+npm install hardhat-gas-reporter --save-dev
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  gasReporter: {
+    enabled: true,
+  }
 };
