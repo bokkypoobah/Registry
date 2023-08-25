8e1c13387debd5d71fdba3e450a84207b54676fb
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-08-26 08:57:56 +1000

    tidy

--- test/Registry.js
+++ test/Registry.js
@@ -204,8 +204,8 @@ describe("Registry", function () {
 
     const collectionData = await data.registry.getCollections(10, 0);
     let i = 0;
-    console.log("       Id Collection Name      Description          Receiver               Owner                  Items AnyUs MintL OMint OBurn Descr Royal Created");
-    console.log("      --- -------------------- -------------------- ---------------------- -------------------- ------- ----- ----- ----- ----- ----- ----- ------------------------");
+    console.log("       Id Collection Name      Description          Receiver             Owner                  Items AnyUs MintL OMint OBurn Descr Royal Created");
+    console.log("      --- -------------------- -------------------- -------------------- -------------------- ------- ----- ----- ----- ----- ----- ----- ------------------------");
     for (const _d of collectionData) {
       const [name, description, owner, receiver, fuses, items, created] = _d;
       if (created == 0) {
@@ -218,7 +218,7 @@ describe("Registry", function () {
       const ownerCanUpdateDescription = (parseInt(fuses) & FUSE_OWNER_CAN_UPDATE_DESCRIPTION) == FUSE_OWNER_CAN_UPDATE_DESCRIPTION ? "y" : "n";
       const ownerCanUpdateRoyalties = (parseInt(fuses) & FUSE_OWNER_CAN_UPDATE_ROYALTIES) == FUSE_OWNER_CAN_UPDATE_ROYALTIES ? "y" : "n";
       console.log("      " + padLeft(i, 3) + " " + padRight(name || '(default)', 20) + " " + padRight(description || '(default)', 20) + " " +
-        padRight(getAccountName(data, receiver), 22) + " " + padRight(getAccountName(data, owner), 20) + " " +
+        padRight(getAccountName(data, receiver), 20) + " " + padRight(getAccountName(data, owner), 20) + " " +
         padLeft(items, 7) + " " +
         padLeft(anyUserCanMintItems, 5) + " " +
         padLeft(minterListCanMintItems, 5) + " " +
